888e45e26a0262fd3d29b2bb6dc104c837f0fedf
홍진서 <hongjinseo@hongjinseos-MacBook-Air.local>
2024-12-29 15:03:27 +0900

    login and auto comment

--- Recomment.py
+++ Recomment.py
@@ -9,7 +9,10 @@ def clickComment():
     driver.find_element(By.NAME, 'mainFrame').send_keys(Keys.SPACE)
     time.sleep(2)
     driver.switch_to.frame("mainFrame")
-    driver.find_element(By.XPATH, '//*[@id="btn_comment_2"]').click()
+    try:
+        driver.find_element(By.XPATH, '//*[@id="btn_comment_2"]').click()
+    except:
+        driver.find_element(By.CLASS_NAME, 'u_cbox_inbox').click()
     time.sleep(2)
 
 def Login(myId, myPw):
@@ -25,7 +28,7 @@ def Login(myId, myPw):
     pyperclip.copy(myId)
     pyautogui.hotkey("command")
     pyautogui.hotkey("command", "v")
-    time.sleep(1)  # 입력 후 잠시 대기
+    time.sleep(1)  
     
     pwBox = driver.find_element(By.ID, "pw")
     pwBox.click()
@@ -37,21 +40,27 @@ def Login(myId, myPw):
     loginBtn.click()
 
     driver.switch_to.window(main_window)
+    time.sleep(1)
 
 
-    #commentBox.send_keys('안녕하세요')
-    #time.sleep(1)
-    #driver.find_element(By.CLASS_NAME, 'u_cbox_txt_upload').click()
+def makeComment(comment):
+    pyperclip.copy(comment)
+    pyautogui.hotkey("command", "v")
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR, '.u_cbox .u_cbox_btn_upload').click()
+    time.sleep(2)
 
-url = "https://blog.naver.com/iknow433/223705744432"
+myId = input("Id를 입력하세요")
+myPw = input("Pw를 입력하세요")
+
+url = "https://blog.naver.com/bbirdsh/223707265629"
 driver = webdriver.Chrome() 
 driver.get(url)
 time.sleep(3)
 
-myId = "iknow433"
-myPw = "6746Kmv+"
+comment = "안녕하세요 이웃님, 좋은 게시글 잘 읽고 갑니다! \n 연말 즐겁게 보내시고 새해 복 많이 받으세요~"
 
 clickComment()
 Login(myId, myPw)
-time.sleep(2)
 clickComment()
+makeComment(comment)
